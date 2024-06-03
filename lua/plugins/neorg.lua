ddc82163704b9b498b021bbba7ddb3fc80d55728
Dênis Rocha <denischarocha@gmail.com>
2024-06-03 10:50:51 -0300

    Neorg Added, Let's make nvim new obsidian

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,14 @@
+return {
+	"vhyrro/luarocks.nvim",
+	priority = 1000, -- We'd like this plugin to load first out of the rest
+	config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+}, {
+	"nvim-neorg/neorg",
+	dependencies = { "luarocks.nvim" },
+	-- put any other flags you wanted to pass to lazy here!
+	config = function()
+		require("neorg").setup({
+			-- put any of your previous config here
+		})
+	end,
+}
